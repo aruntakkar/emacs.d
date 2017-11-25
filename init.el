df4f768a9684c82e2b277edc7d23da534400d602
aruntakkar5@gmail.com <9693884+aruntakkar@users.noreply.github.com>
2017-11-25 18:02:44 +0530

    "usage of defer t for lazy loading, new packages added"

--- init.el
+++ init.el
@@ -10,32 +10,6 @@
   (package-install 'use-package))
 (setq use-package-always-ensure t)
 
-(use-package try
-  :ensure t)
-
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-
-(use-package monokai-theme
-  :ensure t
-  :config (load-theme 'monokai t))
-
-(use-package drag-stuff
-  :ensure t
-  :init (drag-stuff-global-mode 1)
-  :bind (("M-N" . drag-stuff-up)
-	 ("M-P" . drag-stuff-down)))		
-
-(use-package magit
-  :ensure t
-  :defer t
-  :bind ("C-x g" . magit-status))
-  
-(use-package py-autopep8
-  :ensure t
-  :config (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
-
 ;; Basic Configuration
 (setq initial-scratch-message nil)
 (setq inhibit-startup-screen t)
@@ -50,21 +24,48 @@
 (global-set-key "\C-xt" 'recentf-open-files)
 (setq recentf-auto-cleanup 'never)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (monokai)))
- '(custom-safe-themes
-   (quote
-    ("3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
- '(package-selected-packages (quote (py-autopep8 magit ## monokai-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package try
+  :defer t)
+
+(use-package which-key
+  :defer t
+  :diminish which-key-mode
+  :config (which-key-mode))
+
+(use-package monokai-theme
+  :ensure t
+  :config (load-theme 'monokai t))
+
+(use-package drag-stuff
+  :defer t
+  :diminish drag-stuff-mode
+  :config (drag-stuff-global-mode 1)
+  :bind (("M-N" . drag-stuff-up)
+	 ("M-P" . drag-stuff-down)))
+
+(use-package company
+  :defer t
+  :diminish company-mode
+  :init(add-hook 'after-init-hook 'global-company-mode))
+
+(use-package magit
+  :defer t
+  :bind ("C-x g" . magit-status))
+
+(use-package py-autopep8
+  :defer t
+  :init (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
+
+(use-package rainbow-delimiters
+  :defer t
+  :config (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
+
+(use-package rainbow-identifiers
+  :defer t
+  :config (add-hook 'prog-mode-hook 'rainbow-identifiers-mode))
+
+(use-package flycheck
+  :defer t
+  :diminish t
+  :config (global-flycheck-mode))
+    
